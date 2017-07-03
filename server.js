214b0e3a333e9371d6b8a6391c839f0ef47c1d56
CELLAR-D00R <yassine1231997@gmail.com>
2017-07-03 11:36:52 +0200

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 /**
  * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
  */
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 /**
  * Liste des utilisateurs connectés
